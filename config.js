7d1a8504d4b952b2dbad6052cb6bbf596067fda5
norov1 <norov1.android@gmail.com>
2018-09-29 02:23:04 +0400

    connect to mongodb

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    MONGO_URL: 'mongodb://norovone:slanadmin7@ds115533.mlab.com:15533/norovone_db'
+}
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const app = require('./app');
+const database = require('./database');
+const config = require('./config');
 
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+database()
+    .then(info => {
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+        app.listen(config.PORT, () => 
+            console.log(`port ${config.PORT}`)
+        );
+    })
+    .catch(() => {
+        console.error('Unable to connect to database');
+        process.exit(1);
+    });
 
-const arr = ['hello', 'world', 'test'];
 
-app.get('/', (req, res) => res.render('index',{arr: arr}));
-
-app.get('/create', (req, res) => res.render('create'));
-app.post('/create', (req, res) => {
-    console.log(req.body);
-});
-
-app.listen(3000, () => console.log('port 3000'));
